0d543f0ca968cbeb14104a9e33f90da49a17ed14
Susana Valverde <svalverdeh@gmail.com>
2018-07-19 00:09:28 -0400

    galeria en javascrip

--- javascrip/java.js
+++ javascrip/java.js
@@ -1,6 +1,14 @@
-function galery(e){
-    if(e.classList.length == 0)
-    e.classList.add("select")
-    else
-    e.classList.remove("select")
-}
+$(".galeria__img").click(function (e) {
+	var img = e.target.src;
+	var modal = '<div class="modal" id="modal"><img src="'+ img +'" class="modal__img"><div class="modal__boton" id="modal__boton">X</div></div>';
+	$('body').append(modal);
+	$('#modal__boton').click(function(){
+		$('#modal').remove();
+	})
+});
+
+$(document).keyup(function(e){
+	if (e.which==27) {
+		$('#modal').remove();
+	}
+})
